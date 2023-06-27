ce037e745d4ca7a37875c1c0de3903e012cd7ffa
nafeu-khan <nafeu.khan@gmail.com>
2023-06-28 01:11:02 +0600

    cluster update

--- matflow_test/Matflow_Main/modules/feature/cluster.py
+++ matflow_test/Matflow_Main/modules/feature/cluster.py
@@ -40,30 +40,34 @@ def cluster_dataset(file):
     df['Class'] = [cluster_labels[label] for label in kmeans.labels_]
 
 
-    if display_type == "Table":
-        new_value = df.to_dict(orient="records")
-        return JsonResponse(new_value, safe=False)
-    else:
-        # st.pyplot(fig)
-        # Save the plot to a BytesIO stream
-        image_stream = io.BytesIO()
-        plt.savefig(image_stream, format='png', bbox_inches='tight')
-        plt.close(fig)
-        image_stream.seek(0)
+# if display_type == "Table":
+    new_value = df.to_dict(orient="records")
+    # return JsonResponse(new_value, safe=False)
+# else:
+    # st.pyplot(fig)
+    # Save the plot to a BytesIO stream
+    image_stream = io.BytesIO()
+    plt.savefig(image_stream, format='png', bbox_inches='tight')
+    plt.close(fig)
+    image_stream.seek(0)
 
-        # Encode the image stream as base64
-        image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
+    # Encode the image stream as base64
+    image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
 
-        # Create the Plotly graph with the base64-encoded image and increase size
-        graph = go.Figure(go.Image(source=f'data:image/png;base64,{image_base64}'))
-        graph.update_layout(font=dict(family="Arial", size=12), width=1000, height=800,
-                            # xaxis=dict(editable=True),yaxis=dict(editable=True)
-                            )
-        # Convert the graph to HTML and send as a response
-        html_content = pio.to_html(graph, full_html=False)
-        response = HttpResponse(content_type='text/html')
-        response.write(html_content)
+    # Create the Plotly graph with the base64-encoded image and increase size
+    graph = go.Figure(go.Image(source=f'data:image/png;base64,{image_base64}'))
+    graph.update_layout(font=dict(family="Arial", size=12), width=1000, height=800,
+                        # xaxis=dict(editable=True),yaxis=dict(editable=True)
+                        )
+    # Convert the graph to HTML and send as a response
+    html_content = pio.to_html(graph, full_html=False)
+    response = HttpResponse(content_type='text/html')
+    response.write(html_content)
 
-        # Return the graph JSON data
-        graph_json = graph.to_json()
-        return JsonResponse(graph_json, safe=False)
+    # Return the graph JSON data
+    graph_json = graph.to_json()
+    obj= {
+        "table" : new_value,
+        "graph" : graph_json
+    }
+    return JsonResponse(obj, safe=False)
