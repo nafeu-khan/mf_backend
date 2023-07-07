42c7e3c0349a5c32a61b266cd9c3397fadbdaf9e
nafeu-khan <nafeu.khan@gmail.com>
2023-07-07 23:51:10 +0600

    class

--- matflow_test/Matflow_Main/modules/classifier/random_forest.py
+++ matflow_test/Matflow_Main/modules/classifier/random_forest.py
@@ -38,7 +38,11 @@ def hyperparameter_optimization(X_train, y_train,file):
     results_df = pd.DataFrame(results_list)
     results_df = results_df.sort_values(by=['accuracy'], ascending=False)
     best_param = clf.best_params_
-    return JsonResponse(best_param)
+    obj = {
+        "result": results_df,
+        "param": best_param
+    }
+    return JsonResponse(obj)
 
 
 def random_forest(X_train, y_train,file):
