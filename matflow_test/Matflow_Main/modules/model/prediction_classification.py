1a24dc17e5e1c9bd73cdac15003ad74e390fc0ab
nafeu-khan <nafeu.khan@gmail.com>
2023-07-07 19:22:53 +0600

    fjfal

--- matflow_test/Matflow_Main/modules/model/prediction_classification.py
+++ matflow_test/Matflow_Main/modules/model/prediction_classification.py
@@ -94,14 +94,15 @@ def show_result(y, y_pred, result_opt, multi_average,X,model_name):
             template="plotly_white"
         )
         fig_json = pio.to_json(fig)
-        response_data = {'fig': fig_json}
+        response_data = {'graph': fig_json}
         return JsonResponse(response_data)
 
     elif result_opt == "Actual vs. Predicted":
-        graph_header="Actual vs. Predicted Values"
+        graph_header = "Actual vs. Predicted Values"
+        x_range = np.arange(len(y))  # Convert range to a numpy array
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=range(len(y)), y=y, mode='lines', name='Actual'))
-        fig.add_trace(go.Scatter(x=range(len(y_pred)), y=y_pred, mode='lines', name='Predicted'))
+        fig.add_trace(go.Scatter(x=x_range, y=y, mode='lines', name='Actual'))
+        fig.add_trace(go.Scatter(x=x_range, y=y_pred, mode='lines', name='Predicted'))
         fig.update_layout(
             title=graph_header,
             xaxis=dict(title='Index'),
@@ -134,194 +135,180 @@ def show_result(y, y_pred, result_opt, multi_average,X,model_name):
             return JsonResponse(response_data)
 
     elif result_opt == "ROC Curve":
+
         if y.nunique() > 2:
+
             label_encoder = LabelEncoder()
+
             label_encoder.fit(y)
+
             y = label_encoder.transform(y)
+
             classes = label_encoder.classes_
 
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
             min_max_scaler = MinMaxScaler()
+
             X_train_norm = min_max_scaler.fit_transform(X_train)
+
             X_test_norm = min_max_scaler.fit_transform(X_test)
 
-            if (model_name == "RF_Classification"):
+            if model_name == "RF_Classification":
+
                 RF = OneVsRestClassifier(RandomForestClassifier(max_features=0.2))
+
                 RF.fit(X_train_norm, y_train)
+
                 y_pred = RF.predict(X_test_norm)
+
                 pred_prob = RF.predict_proba(X_test_norm)
-            elif (model_name == "MLP_Classification"):
+
+            elif model_name == "MLP_Classification":
+
                 mlp = MLPClassifier(hidden_layer_sizes=(100, 50))
+
                 mlp.fit(X_train_norm, y_train)
+
                 y_pred = mlp.predict(X_test_norm)
+
                 pred_prob = mlp.predict_proba(X_test_norm)
-            elif (model_name == "KNN_Classification"):
+
+            elif model_name == "KNN_Classification":
+
                 k = 5
+
                 knn = KNeighborsClassifier(n_neighbors=k)
+
                 knn.fit(X_train_norm, y_train)
+
                 y_pred = knn.predict(X_test_norm)
+
                 pred_prob = knn.predict_proba(X_test_norm)
-            elif (model_name == "SVM_Classification"):
+
+            elif model_name == "SVM_Classification":
+
                 svm = OneVsRestClassifier(SVC(kernel='linear', probability=True))
+
                 svm.fit(X_train_norm, y_train)
+
                 y_pred = svm.predict(X_test_norm)
+
                 pred_prob = svm.predict_proba(X_test_norm)
-            elif (model_name == "LR_Classification"):
+
+            elif model_name == "LR_Classification":
+
                 lr = OneVsRestClassifier(LogisticRegression(solver='lbfgs'))
+
                 lr.fit(X_train_norm, y_train)
+
                 y_pred = lr.predict(X_test_norm)
+
                 pred_prob = lr.predict_proba(X_test_norm)
-            elif (model_name == "DT_Classification"):
+
+            elif model_name == "DT_Classification":
+
                 dt = OneVsRestClassifier(DecisionTreeClassifier())
+
                 dt.fit(X_train_norm, y_train)
+
                 y_pred = dt.predict(X_test_norm)
+
                 pred_prob = dt.predict_proba(X_test_norm)
+
             else:
-                raise ValueError(
-                    "Invalid model name specified. Please choose one of 'Random Forest Classification', 'Multilayer Perceptron', 'K-Nearest Neighbors', 'Support Vector Machine', 'Logistic Regression', 'Decision Tree Classification'.")
+
+                raise ValueError("Invalid model name specified.")
 
             y_test_binarized = label_binarize(y_test, classes=np.unique(y_test))
 
             fpr = {}
+
             tpr = {}
-            thresh = {}
+
             roc_auc = dict()
 
             n_class = classes.shape[0]
 
             for i in range(n_class):
-                fpr[i], tpr[i], thresh[i] = roc_curve(y_test_binarized[:, i], pred_prob[:, i])
+                fpr[i], tpr[i], _ = roc_curve(y_test_binarized[:, i], pred_prob[:, i])
+
                 roc_auc[i] = auc(fpr[i], tpr[i])
 
-                plt.plot(fpr[i], tpr[i], linestyle='--', label='%s vs Rest (AUC=%0.2f)' % (classes[i], roc_auc[i]))
+            fig = go.Figure()
 
-            plt.plot([0, 1], [0, 1], 'b--')
-            plt.xlim([0, 1])
-            plt.ylim([0, 1.05])
-            plt.title('Multiclass ROC curve')
-            plt.xlabel('False Positive Rate')
-            plt.ylabel('True Positive rate')
-            plt.legend(loc='lower right')
-            st.pyplot(plt.gcf())
-            return
-        # if y.nunique() > 2:
-        #     label_encoder = LabelEncoder()
-        #     label_encoder.fit(y)
-        #     y = label_encoder.transform(y)
-        #     classes = label_encoder.classes_
-        #
-        #     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        #
-        #     min_max_scaler = MinMaxScaler()
-        #     X_train_norm = min_max_scaler.fit_transform(X_train)
-        #     X_test_norm = min_max_scaler.fit_transform(X_test)
-        #
-        #     mlp = MLPClassifier(hidden_layer_sizes=(100, 50))
-        #     mlp.fit(X_train_norm, y_train)
-        #     y_pred = mlp.predict(X_test_norm)
-        #     pred_prob = mlp.predict_proba(X_test_norm)
-        #
-        #     y_test_binarized = label_binarize(y_test, classes=np.unique(y_test))
-        #
-        #     fpr = {}
-        #     tpr = {}
-        #     thresh = {}
-        #     roc_auc = dict()
-        #
-        #     n_class = classes.shape[0]
-        #
-        #     for i in range(n_class):
-        #         fpr[i], tpr[i], thresh[i] = roc_curve(y_test_binarized[:, i], pred_prob[:, i])
-        #         roc_auc[i] = auc(fpr[i], tpr[i])
-        #
-        #         plt.plot(fpr[i], tpr[i], linestyle='--', label='%s vs Rest (AUC=%0.2f)' % (classes[i], roc_auc[i]))
-        #
-        #     plt.plot([0, 1], [0, 1], 'b--')
-        #     plt.xlim([0, 1])
-        #     plt.ylim([0, 1.05])
-        #     plt.title('Multiclass ROC curve')
-        #     plt.xlabel('False Positive Rate')
-        #     plt.ylabel('True Positive rate')
-        #     plt.legend(loc='lower right')
-        #     plt.show()
-        #
-        #     st.pyplot(plt.gcf())
-        #
-        #     return
+            for i in range(n_class):
+                fig.add_trace(
+                    go.Scatter(x=fpr[i], y=tpr[i], mode='lines', name='%s vs Rest (AUC=%0.2f)' % (classes[i], roc_auc[i])))
+
+            fig.add_shape(
+
+                type='line', line=dict(dash='dash'), x0=0, y0=0, x1=1, y1=1
+
+            )
+
+            fig.update_layout(
+
+                title='Multiclass ROC curve',
+
+                xaxis=dict(title='False Positive Rate'),
+
+                yaxis=dict(title='True Positive Rate')
+
+            )
+
+            # Convert fig to JSON-compatible format
+
+            fig_json = pio.to_json(fig)
+
+            # Create a JSON response with the fig_json
+
+            response_data = {'fig': fig_json}
+
+            return JsonResponse(response_data)
+
+
         else:
+
             y_encoded = le.fit_transform(y)
+
             y_pred_encoded = le.transform(y_pred)
 
             fpr, tpr, _ = roc_curve(y_encoded.ravel(), y_pred_encoded.ravel())
 
-            fig, ax = plt.subplots()
+            fig = go.Figure(data=go.Scatter(x=fpr, y=tpr, mode='lines'))
 
-            sns.lineplot(x=fpr, y=tpr,ax=ax)
+            fig.update_layout(
 
-            ax.set_xlabel("False Positive Rate")
+                title='ROC Curve',
 
-            ax.set_ylabel("True Positive Rate")
+                xaxis=dict(title='False Positive Rate'),
 
-            ax.set_title("ROC Curve")
+                yaxis=dict(title='True Positive Rate')
 
-            st.pyplot(fig)
+            )
 
-    # elif result_opt == "Feature Importances":
-    #
-    #     if hasattr(models, 'feature_importances_'):
-    #
-    #         feature_importances = pd.Series(models.feature_importances_, index=X.columns)
-    #
-    #     elif hasattr(models, 'coef_'):
-    #
-    #         feature_importances = pd.Series(models.coef_[0], index=X.columns)
-    #
-    #     else:
-    #
-    #         st.warning("Model doesn't support feature importances")
-    #
-    #         return
-    #
-    #     feature_importances = feature_importances.sort_values(ascending=False)
-    #
-    #     fig, ax = plt.subplots(figsize=(8, 6))
-    #
-    #     sns.barplot(x=feature_importances, y=feature_importances.index, ax=ax)
-    #
-    #     ax.set_xlabel("Feature Importance Score")
-    #
-    #     ax.set_ylabel("Features")
-    #
-    #     ax.set_title("Feature Importances")
-    #
-    #     st.pyplot(fig)
-def actvspred(y,y_pred,graph_header):
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax = sns.lineplot(x=range(len(y)), y=y, label="Actual")
-    ax = sns.lineplot(x=range(len(y_pred)), y=y_pred, label="Predicted")
-    ax.set_xlabel("Index")
-    ax.set_ylabel("Value")
-    ax.set_title("actual vs prediction")
-    plt.close((fig))
-    image_stream = io.BytesIO()
-    image_stream.seek(0)
-    # Encode the image stream as base64
-    image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
-    # Create the Plotly graph with the base64-encoded image and increase size
-    graph = go.Figure(go.Image(source=f'data:image/png;base64,{image_base64}'))
-    graph.update_layout(font=dict(family="Arial", size=12), width=1000, height=800,
-                        # xaxis=dict(editable=True),yaxis=dict(editable=True)
-                        )
-    # Convert the graph to HTML and send as a response
-    html_content = pio.to_html(graph, full_html=False)
-    response = HttpResponse(content_type='text/html')
-    response.write(html_content)
+            # Convert fig to JSON-compatible format
 
-    # Return the graph JSON data
-    graph_json = graph.to_json()
+            fig_json = pio.to_json(fig)
+
+            # Create a JSON response with the fig_json
+
+            response_data = {'fig': fig_json}
+
+            return JsonResponse(response_data)
+
+def actvspred(y, y_pred, graph_header):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=list(range(len(y))), y=y, mode='lines', name='Actual'))
+    fig.add_trace(go.Scatter(x=list(range(len(y_pred))), y=y_pred, mode='lines', name='Predicted'))
+    fig.update_layout(
+        title=graph_header,
+        xaxis=dict(title='Index'),
+        yaxis=dict(title='Value')
+    )
+    graph_json = fig.to_json()
     return graph_json
-
 # def show_multiclass(y, y_pred):
 #
 #     # Binarize the y and y_pred labels
